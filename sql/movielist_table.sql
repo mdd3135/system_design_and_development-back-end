7e7dcc32ad8b97b031c999f680ba654932247842
mdd3135 <313521996@qq.com>
2022-11-02 11:59:48 +0800

    update

---
+++ sql/movielist_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+  `movielist_table` (
+    `movielist_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `movielist_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `movielist_picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `create_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `movie_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `total_movie_count` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    `score/comment_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `total_score_count` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    `total_coment_count` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    `total_score` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    PRIMARY KEY (`movielist_id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
