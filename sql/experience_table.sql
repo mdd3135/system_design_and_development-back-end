a1fe3f629b17bbc472ac06ed3a9c763a4d791eb5
mdd3135 <313521996@qq.com>
2022-11-10 14:58:46 +0800

    update

--- sql/experience_table.sql
+++ sql/experience_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE
     `experience_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `content_md` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
     PRIMARY KEY (`experience_id`)
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
