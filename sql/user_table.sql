c02a4cbdcef3c2e60120a17dc241d4ea02d228a5
mdd3135 <313521996@qq.com>
2022-11-12 19:52:52 +0800

    新增发送邮件验证码来重置密码的功能

--- sql/user_table.sql
+++ sql/user_table.sql
@@ -1,14 +1,16 @@
 CREATE TABLE
   `user_table` (
-    `user_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `user_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `user_password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `sex` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `user_picture` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-    `user_description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `create_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `session_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-    `expiration_time` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-    `user_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '普通',
+    `user_id` varchar(255) NOT NULL,
+    `user_name` varchar(255) NOT NULL,
+    `user_password` varchar(255) NOT NULL,
+    `sex` varchar(255) NOT NULL,
+    `user_picture` varchar(255) NOT NULL DEFAULT '',
+    `user_description` varchar(255) NOT NULL,
+    `create_time` varchar(255) NOT NULL,
+    `session_id` varchar(255) NOT NULL,
+    `expiration_time` varchar(255) NOT NULL DEFAULT '',
+    `user_type` varchar(255) NOT NULL DEFAULT '普通',
+    `user_email` varchar(255) NOT NULL DEFAULT '',
+    `verified_code` varchar(255) NOT NULL DEFAULT '',
     PRIMARY KEY (`user_id`)
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -2,19 +2,29 @@ package com.example.demo;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+// @RunWith(SpringRunner.class)
 public class UserController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    
+    @Autowired
+    MailService mailService;
+
+    @Autowired
+    JavaMailSender mailSender;
 
     @PostMapping("/user_register")
     private Map<String, Object> user_register(@RequestParam Map<String, String> mp){
@@ -146,6 +156,45 @@ public class UserController {
         jdbcTemplate.update(sql);
         return Map.of("code", 0);
     }
+
+    @PostMapping("user_code_send")
+    private Map<String, Object> user_code_send(@RequestParam Map<String, String> mp){
+        String user_id = mp.get("user_id");
+        String sql = "select * from user_table where user_id='" + user_id + "'";
+        List<Map<String, Object>> ls = jdbcTemplate.queryForList(sql);
+        if(ls.size() == 0){
+            return Map.of("code", 3);
+        }
+        String user_email = ls.get(0).get("user_email").toString();
+        String verified_code = "";
+        Random r = new Random();
+        for(int i = 0; i < 6; i++){
+            verified_code = verified_code + r.nextInt(10);
+        }
+        sql = "update user_table set verified_code='" + verified_code + "' where user_id='" + user_id + "'";
+        jdbcTemplate.update(sql);
+        MailController mailController = new MailController();
+        mailController.mySendSimpleMail(user_email, "书影足迹验证码", "验证码：" +  verified_code, mailSender);
+        return Map.of("code", 0);
+    }
+
+    @PostMapping("user_pwd_change")
+    private Map<String, Object> user_pwd_change(@RequestParam Map<String, String> mp){
+        String user_id = mp.get("user_id");
+        String sql = "select * from user_table where user_id='" + user_id + "'";
+        List<Map<String, Object>> ls = jdbcTemplate.queryForList(sql);
+        if(ls.size() == 0){
+            return Map.of("code", 3);
+        }
+        String verified_code = mp.get("verified_code");
+        String true_verified_code = ls.get(0).get("verified_code").toString();
+        if(true_verified_code.compareTo(verified_code) != 0){
+            return Map.of("code", 8);
+        }
+        sql = "update user_table set user_password='" + mp.get("user_password") + "' where user_id='" + user_id + "'";
+        return Map.of("code", 0);
+    }
+    
 
     public static int min(int a, int b){
         if(a < b) return a;
